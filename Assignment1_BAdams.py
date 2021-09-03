c20b27f198ff2763f9c615745b798be637a569e9
BryanA999 <36612137+BryanA999@users.noreply.github.com>
2021-09-02 20:32:00 -0500

    Update Assignment1_BAdams.py
    Added comments

--- Assignment1_BAdams.py
+++ Assignment1_BAdams.py
@@ -3,19 +3,27 @@ from bs4 import BeautifulSoup
 import nltk
 from nltk.corpus import stopwords
 nltk.download('stopwords')
+
+#read article at url
 response = urllib.request.urlopen('https://www.newscientist.com/article/2275587-astronauts-ride-reused-spacex-capsule-and-rocket-for-the-first-time/')
 html = response.read()
+
+#tokenize
 soup = BeautifulSoup(html,"html.parser")
 text = soup.get_text(strip=True)
 tokens = [t for t in text.split()]
+
+#clean tokens of stopwords
 freq = nltk.FreqDist(tokens)
 clean_tokens = tokens[:]
 sr = stopwords.words('english')
 for token in tokens:
     if token in stopwords.words('english'):
         clean_tokens.remove(token)
-    elif tokens.count(token) < 5:
+    elif tokens.count(token) < 5: # ignore tokens that appear less than 5 times
         clean_tokens.remove(token)
+
+#Plot frequency distribution
 freq = nltk.FreqDist(clean_tokens)
 freq.plot(10, cumulative = False)
 
